e4a40166619da2aa6c88ffab94965bf35bcccec3
Dawid Hen <dawid@nitro-digital.co.uk>
2016-01-05 10:27:46 +0100

    Update app script

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -2,32 +2,13 @@ import {bootstrap} from 'angular2/platform/browser'
 import {Component} from 'angular2/core';
 import {FORM_DIRECTIVES} from 'angular2/common'
 
-class PersonDetails{
-    name:string;
-    age:number;
-    job:string;
-    
-    constructor(name:string, age:number, job:string) {
-        this.name = name;
-        this.age = age;
-        this.job = job;
-    }
-}
-var personList = [
-  new PersonDetails('Ja', 12, 'Front'),
-  new PersonDetails('Ty', 22, 'Back'),
-  new PersonDetails('On', 0, 'Solution'),
-];
 @Component({
     selector: 'my-app',
     template: `
-        <h1>{{ listTitle }}</h1>
-
+        <h1>Hello, {{ name }}</h1>
     `
 })
 class AppComponent {   
-    public persons = personList;
-    public listTitle = "Person list";
+    public name = "You";
 }
-
 bootstrap(AppComponent);
